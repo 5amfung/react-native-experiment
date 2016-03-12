b6db67a258ebad594c459b8d339d5fc4bd94bddb
Sam Fung <sam.fung@gmail.com>
2016-03-12 15:02:46 -0800

    Add and link vector icons.

--- app/components/tabbar.ios.js
+++ app/components/tabbar.ios.js
@@ -4,7 +4,7 @@
 
 import React, { Component, TabBarIOS } from 'react-native';
 import { CaptureRoot, NotificationsRoot, ProfileRoot, SearchRoot, StoresRoot } from './tabs';
-
+import MaterialIcon from 'react-native-vector-icons/MaterialIcons';
 
 export default class TabBar extends Component {
 
@@ -23,27 +23,40 @@ export default class TabBar extends Component {
         return this.state.selectedTab === name;
     }
 
-    renderTabBarItem(name, title, icon, Child) {
+    /**
+     * Render the tab bar item.
+     *
+     * @param name a unique name for the tab bar item
+     * @param title a tab title
+     * @param iconName a icon name
+     * @param badge null would not render the badge, otherwise it takes a number or a string
+     * @param Child a child component
+     * @returns {XML}
+     */
+    _renderTabBarItem(name, title, iconName, badge, Child) {
+        // MaterialIcon.TabBarItemIOS is wrapper of React Native's TabBarIOS.Item with
+        // 3 additional settings: iconName, selectedIconName, and iconSize.
         return (
-            <TabBarIOS.Item title={title}
-                            systemIcon={icon}
-                            selected={this._isTabSelected(name)}
-                            onPress={() => {
+            <MaterialIcon.TabBarItemIOS title={title}
+                                        iconName={iconName}
+                                        badge={badge}
+                                        selected={this._isTabSelected(name)}
+                                        onPress={() => {
                                 this._selectTab(name);
                             }}>
                 <Child/>
-            </TabBarIOS.Item>
+            </MaterialIcon.TabBarItemIOS>
         );
     }
 
     render() {
         return (
             <TabBarIOS selectedTab={this.state.selectedTab}>
-                {this.renderTabBarItem('stores', 'Stores', 'featured', StoresRoot)}
-                {this.renderTabBarItem('notifications', 'Notifications', 'featured', NotificationsRoot)}
-                {this.renderTabBarItem('search', 'Search', 'search', SearchRoot)}
-                {this.renderTabBarItem('capture', 'Capture', 'featured', CaptureRoot)}
-                {this.renderTabBarItem('profile', 'Profile', 'featured', ProfileRoot)}
+                {this._renderTabBarItem('stores', 'Stores', 'store', null, StoresRoot)}
+                {this._renderTabBarItem('notifications', 'Notifications', 'notifications', 2, NotificationsRoot)}
+                {this._renderTabBarItem('search', 'Search', 'search', null, SearchRoot)}
+                {this._renderTabBarItem('capture', 'Capture', 'photo-camera', null, CaptureRoot)}
+                {this._renderTabBarItem('profile', 'Profile', 'sentiment-very-satisfied', null, ProfileRoot)}
             </TabBarIOS>
         );
     }
