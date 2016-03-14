9e22a1e535ab5ee8b216e6e650700a2e239afc5f
Sam Fung <sam.fung@gmail.com>
2016-03-13 22:38:07 -0700

    Add authentication reducer.

--- app/reducers/authentication-reducer.js
+++ app/reducers/authentication-reducer.js
@@ -2,6 +2,31 @@
  * Authentication reducer.
  */
 
+import { EMAIL_LOGIN_COMPLETED } from '../actions/email-login-actions';
+import { EMAIL_SIGN_UP_COMPLETED } from '../actions/email-signup-actions';
+import { LOG_OUT_COMPLETED } from '../actions/logout-actions';
 
+const initialState = {
+    authenticated: false,
+    user: null
+};
 
-
+export default function authentication(state = initialState, action) {
+    switch (action.type) {
+        case EMAIL_LOGIN_COMPLETED:
+        case EMAIL_SIGN_UP_COMPLETED:
+            return {
+                ...state,
+                authenticated: true,
+                user: action.user
+            };
+        case LOG_OUT_COMPLETED:
+            return {
+                ...state,
+                authenticated: false,
+                user: null
+            };
+        default:
+            return state;
+    }
+}
--- app/reducers/index.js
+++ app/reducers/index.js
@@ -2,11 +2,13 @@
  * Exports all reducers.
  */
 
+import authentication from './authentication-reducer';
 import emailLogin from './email-login-reducers';
 import emailSignUp from './email-signup-reducers';
 import logout from './logout-reducers';
 
 export {
+    authentication,
     emailLogin,
     emailSignUp,
     logout
