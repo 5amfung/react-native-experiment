dc8c532833d423e747a546881e4b1d433ed30eb9
Sam Fung <sam.fung@gmail.com>
2016-03-13 19:20:17 -0700

    Add remote redux devtool.

--- app/containers/app.js
+++ app/containers/app.js
@@ -3,15 +3,20 @@
  */
 
 import React, { Component } from 'react-native';
-import { applyMiddleware, combineReducers, createStore } from 'redux';
+import { applyMiddleware, combineReducers, compose, createStore } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 
 import * as reducers from '../reducers';
 import TabBar from '../components/tabbar.ios';
 
+// Uncomment devTools() to debug redux states locally.  Not advised for production.
+// import devTools from 'remote-redux-devtools';
 
-let store = createStore(combineReducers(reducers), applyMiddleware(thunk));
+let store = createStore(
+    combineReducers(reducers),
+    compose(applyMiddleware(thunk) /*, devTools() */)
+);
 
 export default class App extends Component {
     render() {
