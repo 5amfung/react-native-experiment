31884715bc3a32cdc4cd2d50d99f8dd785f8e88c
Sam Fung <sam.fung@gmail.com>
2016-03-21 12:44:31 -0700

    Add stack reducer.

--- app/reducers/navigation.js
+++ app/reducers/navigation.js
@@ -5,16 +5,32 @@
 import { NavigationExperimental } from 'react-native';
 const { Reducer } = NavigationExperimental;
 
+let initialState = (key) => {
+    return {
+        key,
+        index: 0,
+        children: [
+            { key }
+        ]
+    };
+};
+
+let getPushedReducerForAction = (action) => {
+    if (action.type === 'push') {
+        return (state) => state || { key: action.key };
+    }
+    return null;
+};
 
 let navigationReducer = Reducer.TabsReducer({
     key: 'navigationState',
     initialIndex: 0,
     tabReducers: [
-        (state) => state || { key: 'stores' },
-        (state) => state || { key: 'notifications' },
-        (state) => state || { key: 'search' },
-        (state) => state || { key: 'capture' },
-        (state) => state || { key: 'profile' }
+        Reducer.StackReducer({ getPushedReducerForAction, initialState: initialState('stores') }),
+        Reducer.StackReducer({ getPushedReducerForAction, initialState: initialState('notifications') }),
+        Reducer.StackReducer({ getPushedReducerForAction, initialState: initialState('search') }),
+        Reducer.StackReducer({ getPushedReducerForAction, initialState: initialState('capture') }),
+        Reducer.StackReducer({ getPushedReducerForAction, initialState: initialState('profile') })
     ]
 });
 export default navigationReducer;
