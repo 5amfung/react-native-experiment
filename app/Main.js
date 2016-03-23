cfbbb176188c512ab7e7027641288b65dbb3e91a
Sam Fung <sam.fung@gmail.com>
2016-03-23 12:16:38 -0700

    Remove unused components. Format code.

--- app/Main.js
+++ app/Main.js
@@ -6,7 +6,7 @@ import React, { Component } from 'react-native';
 import { connect } from 'react-redux';
 import { authenticate } from './actions/authenticate';
 
-import { Actions, Scene, Reducer, Router, TabBar, Modal } from 'react-native-router-flux';
+import { Actions, Scene, Reducer, Router } from 'react-native-router-flux';
 
 import Capture from './components/Capture';
 import Notifications from './components/Notifications';
@@ -19,6 +19,7 @@ import LogIn from './containers/LogIn';
 import SignUp from './containers/SignUp';
 
 
+// We don't need createNavigationReducer.  This is created just to log the action to console.
 const createNavigationReducer = (params) => {
     const defaultReducer = Reducer(params);
     return (state, action)=>{
@@ -39,44 +40,25 @@ class Main extends Component {
             <Router createReducer={createNavigationReducer}>
                 <Scene key='root' hideNavBar={true}>
                     <Scene key='tabbar' tabs={true} default='storesTab'>
-                        <Scene key='storesTab'
-                               title='Stores'
-                               icon={StoreTabIcon}
-                               initial={true}
-                               component={Stores}/>
-                        <Scene key='notificationsTab'
-                               title='Notifications'
-                               icon={NotificationTabIcon}
-                               component={Notifications}/>
-                        <Scene key='searchTab'
-                               title='Search'
-                               icon={SearchTabIcon}
-                               component={Search}/>
-                        <Scene key='captureTab'
-                               title='Capture'
-                               icon={CameraTabIcon}
-                               component={Capture}/>
-                        <Scene key='profileTab'
-                               title='Profile'
-                               icon={ProfileTabIcon}
-                               component={Profile}/>
+                        <Scene key='storesTab' title='Stores' icon={StoreTabIcon}
+                               initial={true} component={Stores}/>
+                        <Scene key='notificationsTab' title='Notifications'
+                               icon={NotificationTabIcon} component={Notifications}/>
+                        <Scene key='searchTab' title='Search'
+                               icon={SearchTabIcon} component={Search}/>
+                        <Scene key='captureTab' title='Capture'
+                               icon={CameraTabIcon} component={Capture}/>
+                        <Scene key='profileTab' title='Profile'
+                               icon={ProfileTabIcon} component={Profile}/>
                     </Scene>
                     <Scene key='login' direction='vertical'>
-                        <Scene key="loginModal"
-                               title="Log In"
-                               component={LogIn}
-                               schema='modal'
-                               leftTitle='Cancel'
+                        <Scene key="loginModal" title="Log In" component={LogIn} schema='modal' leftTitle='Cancel'
                                onLeft={() => {
                                    Actions.pop();
                                }}/>
                     </Scene>
                     <Scene key='signUp' direction='vertical'>
-                        <Scene key="signUpModal"
-                               title="Sign Up"
-                               component={SignUp}
-                               schema='modal'
-                               leftTitle='Cancel'
+                        <Scene key="signUpModal" title="Sign Up" component={SignUp} schema='modal' leftTitle='Cancel'
                                onLeft={() => {
                                    Actions.pop();
                                }}/>
