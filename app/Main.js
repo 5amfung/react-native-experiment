aaab40fab99db751123f0a45692e5f75f64e8f1c
Sam Fung <sam.fung@gmail.com>
2016-03-22 17:52:43 -0700

    Refactor router to Main.

--- app/Main.js
+++ app/Main.js
@@ -2,13 +2,30 @@
  * Main.
  */
 
-import React, { Component, NavigationExperimental } from 'react-native';
-const { Reducer } = NavigationExperimental;
-
+import React, { Component } from 'react-native';
 import { connect } from 'react-redux';
 import { authenticate } from './actions/authenticate';
-import TabBar from './components/TabBar.ios';
 
+import { Actions, Scene, Reducer, Router, TabBar, Modal } from 'react-native-router-flux';
+
+import Capture from './components/Capture';
+import Notifications from './components/Notifications';
+import Profile from './components/Profile';
+import Search from './components/Search';
+import Stores from './components/Stores';
+import { StoreTabIcon, NotificationTabIcon, SearchTabIcon, CameraTabIcon, ProfileTabIcon } from './components/TabIcon';
+
+import LogIn from './containers/LogIn';
+import SignUp from './containers/SignUp';
+
+
+const createNavigationReducer = (params) => {
+    const defaultReducer = Reducer(params);
+    return (state, action)=>{
+        console.log('ACTION:', action);
+        return defaultReducer(state, action);
+    };
+};
 
 class Main extends Component {
 
@@ -19,31 +36,65 @@ class Main extends Component {
 
     render() {
         return (
-            <TabBar navigationState={this.props.navigationState}
-                    selectTabIndex={this.props.selectTabIndex}/>
+            <Router createReducer={createNavigationReducer}>
+                <Scene key='root' hideNavBar={true}>
+                    <Scene key='tabbar' tabs={true} default='storesTab'>
+                        <Scene key='storesTab'
+                               title='Stores'
+                               icon={StoreTabIcon}
+                               initial={true}
+                               component={Stores}/>
+                        <Scene key='notificationsTab'
+                               title='Notifications'
+                               icon={NotificationTabIcon}
+                               component={Notifications}/>
+                        <Scene key='searchTab'
+                               title='Search'
+                               icon={SearchTabIcon}
+                               component={Search}/>
+                        <Scene key='captureTab'
+                               title='Capture'
+                               icon={CameraTabIcon}
+                               component={Capture}/>
+                        <Scene key='profileTab'
+                               title='Profile'
+                               icon={ProfileTabIcon}
+                               component={Profile}/>
+                    </Scene>
+                    <Scene key='login' direction='vertical'>
+                        <Scene key="loginModal"
+                               title="Log In"
+                               component={LogIn}
+                               schema='modal'
+                               leftTitle='Cancel'
+                               onLeft={() => {
+                                   Actions.pop();
+                               }}/>
+                    </Scene>
+                    <Scene key='signUp' direction='vertical'>
+                        <Scene key="signUpModal"
+                               title="Sign Up"
+                               component={SignUp}
+                               schema='modal'
+                               leftTitle='Cancel'
+                               onLeft={() => {
+                                   Actions.pop();
+                               }}/>
+                    </Scene>
+                </Scene>
+            </Router>
         );
     }
 }
 Main.propTypes = {
-    authenticate: React.PropTypes.func,
-    navigationState: React.PropTypes.object,
-    selectTabIndex: React.PropTypes.func
-};
-
-let mapStateToProps = state => {
-    return {
-        navigationState: state.navigation
-    };
+    authenticate: React.PropTypes.func
 };
 
 let mapDispatchToProps = dispatch => {
     return {
         authenticate: () => {
             dispatch(authenticate());
-        },
-        selectTabIndex: (index) => {
-            dispatch(Reducer.TabsReducer.JumpToAction(index));
         }
     };
 };
-export default connect(mapStateToProps, mapDispatchToProps)(Main);
+export default connect(undefined, mapDispatchToProps)(Main);
--- app/app.js
+++ app/app.js
@@ -8,18 +8,7 @@ import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import devTools from 'remote-redux-devtools';
 import * as reducers from './reducers';
-
-import { Actions, Scene, Reducer, Router, TabBar, Modal } from 'react-native-router-flux';
-
-import Capture from './components/Capture';
-import Notifications from './components/Notifications';
-import Profile from './components/Profile';
-import Search from './components/Search';
-import Stores from './components/Stores';
-import { StoreTabIcon, NotificationTabIcon, SearchTabIcon, CameraTabIcon, ProfileTabIcon } from './components/TabIcon';
-
-import LogIn from './containers/LogIn';
-import SignUp from './containers/SignUp';
+import Main from './Main';
 
 
 const store = createStore(
@@ -27,66 +16,11 @@ const store = createStore(
     compose(applyMiddleware(thunk), devTools())  // Comment out devTools() for production.
 );
 
-const createNavigationReducer = (params) => {
-    const defaultReducer = Reducer(params);
-    return (state, action)=>{
-        console.log('ACTION:', action);
-        return defaultReducer(state, action);
-    };
-};
-
-
 export default class App extends Component {
     render() {
         return (
             <Provider store={store}>
-                <Router createReducer={createNavigationReducer}>
-                    <Scene key='root' hideNavBar={true}>
-                        <Scene key='tabbar' tabs={true} default='storesTab'>
-                            <Scene key='storesTab'
-                                   title='Stores'
-                                   icon={StoreTabIcon}
-                                   initial={true}
-                                   component={Stores}/>
-                            <Scene key='notificationsTab'
-                                   title='Notifications'
-                                   icon={NotificationTabIcon}
-                                   component={Notifications}/>
-                            <Scene key='searchTab'
-                                   title='Search'
-                                   icon={SearchTabIcon}
-                                   component={Search}/>
-                            <Scene key='captureTab'
-                                   title='Capture'
-                                   icon={CameraTabIcon}
-                                   component={Capture}/>
-                            <Scene key='profileTab'
-                                   title='Profile'
-                                   icon={ProfileTabIcon}
-                                   component={Profile}/>
-                        </Scene>
-                        <Scene key='login' direction='vertical'>
-                            <Scene key="loginModal"
-                                   title="Log In"
-                                   component={LogIn}
-                                   schema='modal'
-                                   leftTitle='Cancel'
-                                   onLeft={() => {
-                                       Actions.pop();
-                                   }}/>
-                        </Scene>
-                        <Scene key='signUp' direction='vertical'>
-                            <Scene key="signUpModal"
-                                   title="Sign Up"
-                                   component={SignUp}
-                                   schema='modal'
-                                   leftTitle='Cancel'
-                                   onLeft={() => {
-                                       Actions.pop();
-                                   }}/>
-                        </Scene>
-                    </Scene>
-                </Router>
+                <Main/>
             </Provider>
         );
     }
